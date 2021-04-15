c09034061518a19edaf53e65939353ab7259de9f
Sauchenkoav <sauchenkoav@gmail.com>
2021-04-15 14:38:23 +0300

    add login page test

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
+class LoginPageLocators():
+    LOGINPAGE_URL="http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    LOGINPAGE_LOGINFORM = (By.CSS_SELECTOR, "#login_form")
+    LOGINPAGE_REGISTERFORM = (By.CSS_SELECTOR, "#register_form")
